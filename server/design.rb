263ff89a76f7ef1af2afa2c18ece4ff358bf8c8d
Juergen 'eTM' Mangler <juergen.mangler@gmail.com>
2020-02-13 00:15:11 +0100

    more sse tests

--- server/design.rb
+++ server/design.rb
@@ -20,11 +20,30 @@ require 'rubygems'
 require 'json'
 require 'riddl/server'
 require 'riddl/client'
+require 'fileutils'
 
 class GetList < Riddl::Implementation
   def response
     names = Dir.glob(File.join(__dir__,'models','*.xml')).map do |f|
       File.basename(f)
+    end
+    Riddl::Parameter::Complex.new('list','application/json',JSON::pretty_generate(names))
+  end
+end
+
+class Create < Riddl::Implementation
+  def response
+    name = @p[0].value
+    fname = File.join('models',name + '.xml')
+    FileUtils.cp('testset.xml',fname)
+    XML::Smart::modify(fname) do |doc|
+      doc.register_namespace 'p', 'http://riddl.org/ns/common-patterns/properties/1.0'
+      doc.find('/testset/attributes/p:info').each do |ele|
+        ele.value = name
+      end
+      doc.find('/testset/attributes/p:author').each do |ele|
+        ele.value = @h['DN'] + ' ' + @h['SN']
+      end
     end
     Riddl::Parameter::Complex.new('list','application/json',JSON::pretty_generate(names))
   end
@@ -55,7 +74,24 @@ end
 
 class PutItem < Riddl::Implementation
   def response
-    p @r
+    cont = @p[0].value.read
+    XML::Smart.string(cont) do |doc|
+      doc.register_namespace 'p', 'http://riddl.org/ns/common-patterns/properties/1.0'
+      doc.find('/testset/attributes/p:author').each do |ele|
+        ele.value = @h['DN'] + ' ' + @h['SN']
+      end
+    end
+  end
+end
+
+class Active < Riddl::SSEImplementation
+  def onopen
+    @active = @a[0]
+    @name = @r.last
+    @active[@name] = File.read(File.join('models',@name + '.xml.active')) rescue nil
+  end
+  def onclose
+    @active.delete(@r[0])
   end
 end
 
@@ -63,11 +99,15 @@ server = Riddl::Server.new(File.join(__dir__,'/design.xml'), :host => 'localhost
   accessible_description true
   cross_site_xhr true
 
+  @riddl_opts[:active] = {}
+
   on resource do
     run GetList if get
+    run Create if post 'name'
     on resource '[a-zA-Z0-9öäüÖÄÜ _-]+\.xml' do
       run GetItem, @riddl_opts[:instantiate], @riddl_opts[:cockpit] if get
       run PutItem if put 'content'
+      run Active, @riddl_opts[:active] if sse
     end
   end
 end.loop!
